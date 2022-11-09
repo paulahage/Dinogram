f143dc821224fb53507e21be6c7eb87200255fe2
paula hage <hage.paula@hotmail.com>
2022-11-09 15:25:47 +0100

    #02 fix SidebarCompact

--- website/src/components/SharedLayout.js
+++ website/src/components/SharedLayout.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import SidebarCompact from "./Sidebar_Compact";
 
+import SidebarCompact from './SidebarCompact'
 
 const SharedLayout = () => {
   return (
