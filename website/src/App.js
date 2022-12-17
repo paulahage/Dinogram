2c8d4334d399719cdf7c0af01dab198d0bf81bdb
paula hage <hage.paula@hotmail.com>
2022-12-17 15:51:43 +0100

    #22 changed the URLS to match Instagram URL

--- website/src/App.js
+++ website/src/App.js
@@ -14,9 +14,9 @@ function App() {
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<Home />} />
           <Route path="/search" element={<Search />} />
-          <Route path="/direct_message" element={<DirectMessage/>}/>
+          <Route path="/direct/inbox" element={<DirectMessage/>}/>
           <Route path="/:user" element={<Profile />} />
-          <Route path="/saved_posts" element={<SavedPosts/>}/>
+          <Route path="/:userId/saved" element={<SavedPosts/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
--- website/src/pages/SavedPosts.js
+++ website/src/pages/SavedPosts.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import styled from "styled-components";
 
 const SavedPosts = () => {
+  const { userId } = useParams();
+
   return <SavedPostsWrapper>SavedPosts</SavedPostsWrapper>;
 };
 
